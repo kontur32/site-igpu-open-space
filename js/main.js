50265aef04f9e5bf76cb8cf0d86efcd875a85776
S <kontur32@yandex.ru>
2021-01-17 03:48:17 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ $(document).ready(function(){
  		scrollTop:$("#" + $(this).data('value')).offset().top
  	},1000)
   
- })
+ });
+
+ $(".nav-button button").click(function(){
+	$("body,html").animate({
+		scrollTop:$("#" + $(this).data('value')).offset().top
+	},1000)
+ 
+});
 
 })
